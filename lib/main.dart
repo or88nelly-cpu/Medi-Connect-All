f096269ca553909b8f417f792608c919c2ff902d
Likhin Nelliyotan <137990556+likhinnelliyotan07@users.noreply.github.com>
2026-07-15 16:43:49 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,34 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:medi_connect/core/constants/app_router.dart';
+import 'package:medi_connect/core/constants/env_config.dart';
 import 'package:medi_connect/core/functions/app_responsive.dart';
 import 'package:medi_connect/core/theme/app_theme.dart';
 import 'package:medi_connect/core/theme/theme_cubit.dart';
 
 import 'package:medi_connect/boot_strap/services/app_initializer.dart';
 import 'package:medi_connect/core/dependency_injection/app_providers.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
-  await AppInitializer.init();
-  runApp(const MyApp());
+  // It is important to run WidgetsFlutterBinding.ensureInitialized()
+  // within the same zone as runApp. SentryFlutter.init creates a new zone.
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = EnvConfig.sentryDsn;
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () async {
+      await AppInitializer.init();
+     
+      runApp(SentryWidget(child: const MyApp()));
+    },
+  );
 }
 
 class MyApp extends StatefulWidget {
